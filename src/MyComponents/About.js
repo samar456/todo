fbf1b5631de1ecfb30f437ecb10b0faec1c7b4dd
samar456 <84350876+samar456@users.noreply.github.com>
2023-07-04 01:18:55 +0530

    Update About.js

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -3,8 +3,26 @@ import React from 'react'
 export const About = () => {
     return (
         <div>
-            This is an about component 
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum corporis odio, nesciunt sed eveniet, nemo amet, dignissimos magnam itaque ipsam possimus. Inventore vitae, perferendis harum quas cupiditate adipisci repellendus tempore asperiores at consequatur quos?</p>
-        </div>
+      This is about TODO Web Application
+      <p>
+        <ol>
+          <li>
+            Task Creation: Allow users to create new tasks by entering a task
+            title, description, due date, and any additional relevant
+            information.
+          </li>
+          <li>
+            Task Organization: Provide options for users to categorize and
+            organize their tasks into different lists or categories, such as
+            work, personal, or shopping.
+          </li>
+          <li>Task Prioritization: Enable users to set task priorities, such as high, medium, or low, to help them focus on important tasks and manage their time effectively.</li>
+          <li>Task Editing and Updating: Allow users to edit and update task details, such as modifying the title, description, due date, or priority level.</li>
+          <li>Task Completion: Provide a mechanism for users to mark tasks as completed or mark them as unfinished if they need to be revisited.</li>
+          <li>Task Deletion: Allow users to delete tasks that are no longer needed or relevant.</li>
+          
+        </ol>
+      </p>
+    </div>
     )
 }
